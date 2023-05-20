8377afff60309f48894dd4935d6462b9a25c8244
Krassy Krastev <133277812+krassykrastev@users.noreply.github.com>
2023-05-20 11:45:27 +0200

    Update guss_a_number.py

--- guss_a_number.py
+++ guss_a_number.py
@@ -8,7 +8,12 @@ while True:
     player_number = int(player_input)
     if player_number == computer_number:
         print('You guess it!')
-        break
+        play_again = input('Do you want to play again? (Y/N)')
+        if play_again.upper() == 'N':
+            break
+        if play_again.upper() == 'Y':
+            computer_number = random.randint(1, 100)
+            continue
     elif player_number > computer_number:
         print('Too High!')
     elif player_number < computer_number:
